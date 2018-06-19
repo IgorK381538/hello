2d81692560da8153694d1be9fe68f5aa46327501
IgorK381538 <38142368+IgorK381538@users.noreply.github.com>
2018-06-19 21:28:50 +0300

    Update and rename ml2.py to ML.py

--- aiML/ML.py
+++ aiML/ML.py
@@ -5,25 +5,32 @@ import re
 import os
 import codecs
 from sklearn import feature_extraction
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import mpld3
+from sklearn.feature_extraction import image
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.cluster import KMeans
+from IPython import get_ipython
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem import PorterStemmer
+from pymystem3 import Mystem
 
+print('Выбирите стеммер 1 - Porter, 2 - Snowball ')
+j = int(input())
+if j == 2:
+    stemmer = SnowballStemmer("russian")
+elif j == 1:
+    stemmer = PorterStemmer()
 
 dataset = pd.read_csv(r"data.csv")
 dataset = dataset['keyword']
 
 i = 0
-for line in dataset:
-    line = re.sub(r'(\<(/?[^>]+)>)', ' ', line)
-    line = re.sub('[^а-яА-Я ]', '', line)
-    dataset[i] = line
+while i < len(dataset):
+    dataset[i] = re.sub(r'(\<(/?[^>]+)>)', ' ', dataset[i])
+    dataset[i] = re.sub('[^а-яА-Я ]', '', dataset[i])
     i += 1
 
-from nltk.stem.snowball import SnowballStemmer
-stemmer = SnowballStemmer("russian")
-
-def token_and_stem(text):
+def tns(text):
     tokens = [word for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
     filtered_tokens = []
     for token in tokens:
@@ -32,9 +39,7 @@ def token_and_stem(text):
     stems = [stemmer.stem(t) for t in filtered_tokens]
     return stems
 
-token_and_stem('sad')
-
-def token_only(text):
+def to(text):
     tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
     filtered_tokens = []
     for token in tokens:
@@ -45,39 +50,43 @@ def token_only(text):
 totalvocab_stem = []
 totalvocab_token = []
 for i in dataset:
-    allwords_stemmed = token_and_stem(i)
+    allwords_stemmed = tns(i)
     totalvocab_stem.extend(allwords_stemmed)
     
-    allwords_tokenized = token_only(i)
+    allwords_tokenized = to(i)
     totalvocab_token.extend(allwords_tokenized)
 
 stopwords = nltk.corpus.stopwords.words('russian')
-stopwords.extend(['что', 'это', 'так', 'вот', 'быть', 'как', 'в', 'к', 'на'])
-
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+stopwords.extend([])
 
 n_featur=200000
 tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=10000,
                                  min_df=0.01, stop_words=stopwords,
-                                 use_idf=True, tokenizer=token_and_stem, ngram_range=(1,3))
-from IPython import get_ipython
+                                 use_idf=True, tokenizer=tns, ngram_range=(1,3))
 ipython = get_ipython()
 
 tfidf_matrix = tfidf_vectorizer.fit_transform(dataset)
 num_clusters = 15
 
-from sklearn.cluster import KMeans
-
 km = KMeans(n_clusters=num_clusters)
 km.fit(tfidf_matrix)
 idx = km.fit(tfidf_matrix)
 clusters = km.labels_.tolist()
-clusterkm = km.labels_.tolist()
+
+mbk  = MiniBatchKMeans(init='random', n_clusters=num_clusters)
+mbk.fit_transform(tfidf_matrix)
+miniclusters = mbk.labels_.tolist()
 
 dataout = dataset.tolist()
-frame = pd.DataFrame(dataout, index = [clusterkm])
-out = { 'words': dataout, '№ of cluster': clusterkm }
+frame = pd.DataFrame(dataout, index = [clusters])
+out = { 'words': dataout, '№ of cluster': clusters }
 resultDF = pd.DataFrame(out, columns = ['words', '№ of cluster'])
-resultDF.to_csv("clust.txt", sep='\t', encoding='utf-8')
+resultDF.to_csv("clustkm.txt", sep='\t', encoding='utf-8')
+
+dataout = dataset.tolist()
+frame = pd.DataFrame(dataout, index = [miniclusters])
+out = { 'words': dataout, '№ of cluster': miniclusters }
+resultDF = pd.DataFrame(out, columns = ['words', '№ of cluster'])
+resultDF.to_csv("clustmkm.txt", sep='\t', encoding='utf-8')
 
 
